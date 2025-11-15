bb5ede9ff653b6b0908a382e42b497113f8478f3
vipin-kumar735 <patelharimohan31@gmail.com>
2025-11-15 23:19:37 +0530

    Add files via upload

---
+++ Rosevipchat/__init__.py
@@ -0,0 +1,5 @@
+# This file makes EsproAiChat a Python package
+# You can optionally import main bot here if needed
+
+from .Espro import app
+from .start import app # Uncomment if you want to import bot app directly
