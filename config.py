c4aef6889923ef3624c331fd2b41e7046d064f86
vipin-kumar735 <patelharimohan31@gmail.com>
2025-11-15 23:55:37 +0530

    🍁𝐕𝐈𝐏 𝐂𝐇𝐎𝐑 🍁

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-ESPRO_API_KEY = os.environ.get("ESPRO_API_KEY")
-ESPRO_API_URL = os.environ.get(
-    "ESPRO_API_URL",
+Rose_API_KEY = os.environ.get("Rose_API_KEY")
+Rose_API_URL = os.environ.get(
+    "Rose_API_URL",
     "https://esproapi-9e00fce70d45.herokuapp.com"
 )
 DEFAULT_MODEL = os.environ.get("DEFAULT_MODEL", "")
