b63dfde193ccad4e312757f79843e185a00ef90d
vipin-kumar735 <patelharimohan31@gmail.com>
2025-11-15 23:28:10 +0530

    🍁𝐕𝐈𝐏 𝐂𝐇𝐎𝐑 🍁

--- Rosevipchat/__init__.py
+++ Rosevipchat/__init__.py
@@ -1,5 +1,5 @@
-# This file makes EsproAiChat a Python package
+# This file makes RoseAiChat a Python package
 # You can optionally import main bot here if needed
-
-from .Espro import app
+#power by @Vip_chor support channel @TEAM_CHOR
+from .Rose import app
 from .start import app # Uncomment if you want to import bot app directly
